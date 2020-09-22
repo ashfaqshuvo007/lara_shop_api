149011a282f2f3c697c42b60c8917685620d6b16
Ashfaq H Ahmed <ashfaqshuvo14@gmail.com>
2020-09-22 15:53:12 +0600

    Created New tables. Added new Models for products

---
+++ app/Http/Controllers/ProductFeaturesController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\ProductFeatures;
+use Illuminate\Http\Request;
+
+class ProductFeaturesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\ProductFeatures  $productFeatures
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ProductFeatures $productFeatures)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\ProductFeatures  $productFeatures
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ProductFeatures $productFeatures)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\ProductFeatures  $productFeatures
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ProductFeatures $productFeatures)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\ProductFeatures  $productFeatures
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ProductFeatures $productFeatures)
+    {
+        //
+    }
+}
--- database/migrations/2020_09_20_052126_create_products_table.php
+++ database/migrations/2020_09_20_052126_create_products_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use phpDocumentor\Reflection\Types\Nullable;
 
 class CreateProductsTable extends Migration
 {
@@ -15,6 +16,13 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('feature_image')->nullable();
+            $table->string('price');
+            $table->string('qty');
+            $table->tinyInteger('is_published')->default(1);
+            $table->unsignedbigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
             $table->timestamps();
         });
     }
